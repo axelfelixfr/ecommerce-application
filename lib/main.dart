e67d759b7691f82c154dad93d7c6b09380c71449
Axel <axelfelix123@hotmail.com>
2021-12-04 19:55:04 -0600

    Paginas asignadas para la app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppEcommerce());
+}
+
+class AppEcommerce extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecommerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Text('Hola mundo!'),
+    );
+  }
+}
